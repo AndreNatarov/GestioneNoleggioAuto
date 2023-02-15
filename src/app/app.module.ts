8527461bdd6f201f849fae51698a749901331e24
Sus <bvehap46@gmail.com>
2023-02-15 12:37:32 +0100

    feature & fix: Aggiunta cartella modules e al suo interno il module veicolo, rimosso il component auto da components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/components.component';
-import { AutoComponent } from './components/auto/auto.component';
 import { AboutComponent } from './components/about/about.component';
 import { DettaglioAutoComponent } from './components/dettaglio-auto/dettaglio-auto.component';
 import { RicercaComponent } from './components/ricerca/ricerca.component';
 import { HomeComponent } from './components/home/home.component';
 import { PreviewComponent } from './components/preview/preview.component';
 import { ServicesComponent } from './components/services/services.component';
+import { ModulesComponent } from './components/modules/modules.component';
+import { VeicoloComponent } from './components/modules/veicolo/veicolo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentsComponent,
-    AutoComponent,
     AboutComponent,
     DettaglioAutoComponent,
     RicercaComponent,
     HomeComponent,
     PreviewComponent,
-    ServicesComponent
+    ServicesComponent,
+    ModulesComponent,
+    VeicoloComponent
   ],
   imports: [
     BrowserModule,
