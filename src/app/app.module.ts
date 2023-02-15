4893fad5b968029209f97ad1b42936b595125ab7
Sus <bvehap46@gmail.com>
2023-02-15 11:46:07 +0100

    feature: Aggiunta componenti about, auto e dettaglio-auto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/components.component';
+import { AutoComponent } from './components/auto/auto.component';
+import { ChiSiamoComponent } from './components/chi-siamo/chi-siamo.component';
+import { AboutComponent } from './components/about/about.component';
+import { DettaglioAutoComponent } from './components/dettaglio-auto/dettaglio-auto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent
+    ComponentsComponent,
+    AutoComponent,
+    ChiSiamoComponent,
+    AboutComponent,
+    DettaglioAutoComponent
   ],
   imports: [
     BrowserModule,
