145deae1db9a3417e969985d877952d417f08094
Andre_Natarov <andrenatarovmauri@gmail.com>
2023-02-15 16:04:44 +0100

    Aggiunto veicolo.ts

---
+++ src/app/models/veicolo.ts
@@ -0,0 +1,15 @@
+export class Veicolo{
+
+    constructor(
+
+        public marca:string,
+        public modello:string,
+        public tipologia:string,
+        public prezzoGiornaliero:number,
+        public optional:string,
+        public chilometraggio:number,
+        public immagine:string,
+
+    ){}
+
+}
