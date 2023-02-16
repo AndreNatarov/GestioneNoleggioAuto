7148ba3099cedd60bfa7b074f683ab1b41ba8135
Andre_Natarov <andrenatarovmauri@gmail.com>
2023-02-16 09:01:50 +0100

    Aggiornato veicolo.ts

--- src/app/models/veicolo.ts
+++ src/app/models/veicolo.ts
@@ -5,14 +5,19 @@ export class Veicolo{
         public tagTendenza: boolean,
         public marca:string,
         public modello:string,
+        public nuimeroPosti:number,
+        public cavalli:number,
+        public cilindrata:number,
+        public carburante:number,
         public prezzoGiornaliero:number,
-        public optional:string,         //Come gestiamo i vari optional? Valore da mettere in un array che corrisponde ad un specifico optional?
+        public optional1:string,
+        public optional2:string,
+        public optional3:string,
         public chilometraggio:number,
-        public tagVeicolo:string,   //Sostituire valori in valori numerici
-        public tagNazione:string,
+        public tagVeicolo:number,       //0 = auto, 1 = moto, 2 = camion 
+        public tagNazione:number,       //0 = Europea, 1 = Giapponese, 2 = Americana
         public patente:string, 
         public immagine:boolean,    //true = DX, false = SX
-        //Bisogna aggiungere specifiche del veicolo (array di array?)
         
     ){}
 
