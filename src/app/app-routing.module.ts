b84f4585aaded7137dee0914e2b8930bafe727a6
Andre_Natarov <andrenatarovmauri@gmail.com>
2023-02-16 15:31:01 +0100

    Corretta route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
 
-{path : '', component}
+{path : '', component : HomeComponent},
+//{path : 'noleggio', component : }
 
 ];
 
