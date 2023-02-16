23e497b36a60926ae5c1b8c90e4a67021fd4d1cf
vehap24 <bvehap46@gmail.com>
2023-02-16 23:07:04 +0100

    test commit aiuto grazie arrivedercio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { NoleggioComponent } from './components/noleggio/noleggio.component';
 
 const routes: Routes = [
 
 {path : '', component : HomeComponent},
-{ path : 'noleggio', component : NoleggioComponent}
+{ path : 'noleggio', component : NoleggioComponent},
+{ path : 'about', component : AboutComponent}
 
 ];
 
