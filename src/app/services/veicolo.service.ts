d2757875260a8659d73ece3aebe3ecbc15bc2ef1
Andre_Natarov <andrenatarovmauri@gmail.com>
2023-02-16 09:18:45 +0100

    Aggiunta CBR 1000

--- src/app/services/veicolo.service.ts
+++ src/app/services/veicolo.service.ts
@@ -9,7 +9,19 @@ export class VeicoloService{
             true,
             'Honda',
             'CBR 1000',
-
+            2,
+            217.5,
+            999,
+            0,
+            250,
+            'GPS',
+            'Cambio Elettronico',
+            'Manopole riscaldate',
+            32495,
+            1,
+            1,
+            'A',
+            true,
 
         )[{}
 
