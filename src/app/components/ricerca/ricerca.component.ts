7991356f23d4dab957aa3183dd63cb75b721f634
Sus <bvehap46@gmail.com>
2023-02-17 12:06:15 +0100

    feat: Aggiunta descrizione a ogni card

--- src/app/components/ricerca/ricerca.component.ts
+++ src/app/components/ricerca/ricerca.component.ts
@@ -1,11 +1,12 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-
+import { Component, EventEmitter, Output} from '@angular/core';
+import { Veicolo } from 'src/app/models/veicolo';
+import { VeicoloService } from 'src/app/services/veicolo.service';
 @Component({
   selector: 'app-ricerca',
   templateUrl: './ricerca.component.html',
   styleUrls: ['./ricerca.component.css']
 })
-export class RicercaComponent {
+export class RicercaComponent{
   @Output() cercaEvent = new EventEmitter();
   
   valore = '';
@@ -14,4 +15,10 @@ export class RicercaComponent {
   { 
     this.cercaEvent.emit(this.valore);   
   }
+
+  veicoli:Veicolo[];
+  constructor(private service:VeicoloService){
+    this.veicoli = service.getAll();
+  }
+
 }
